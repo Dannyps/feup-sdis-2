b8ede2501598be342087247827f48205768af908
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-05-30 17:26:46 +0100

    Implemented async IO for reading the file to be backed up

--- src/service/RMIListen.java
+++ src/service/RMIListen.java
@@ -1,9 +1,13 @@
 package service;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -43,22 +47,54 @@ public class RMIListen implements RMIInterface {
 
     @Override
     public Boolean backup(String filename, int replicationDegree) throws RemoteException {
-        File file = new File(filename);
-        byte[] content = new byte[0];
-
+        // hacky way to create a dynamic array of byte
+        ByteArrayOutputStream fileContentStream = new ByteArrayOutputStream();
+        byte[] fileContent;
         try {
-            content = Files.readAllBytes(file.toPath());
+            // Creates a random access file stream to read from
+            RandomAccessFile randomAccessStream = new RandomAccessFile(filename, "r");
+            FileChannel inputFileChannel = randomAccessStream.getChannel();
+            ByteBuffer buf = ByteBuffer.allocate(2048);
+
+            // while it doen't reach end of stream, read from the file channel into byte
+            // buffer
+            int readBytes;
+            while ((readBytes = inputFileChannel.read(buf)) != -1) {
+                // flip buffer from writing mode to reading mode
+                buf.flip();
+                // move the data from the buffer into the byte array
+                byte[] auxiliarBuf = new byte[2048];
+                buf.get(auxiliarBuf, 0, readBytes);
+                PrintMessage.i("it worked", "Read " + readBytes + " so far");
+                // append to fileContentStream
+                fileContentStream.write(auxiliarBuf, 0, readBytes);
+                // mark buffer as ready for further writing (aka mark as empty)
+                buf.clear();
+            }
+
+            // close all streams
+            randomAccessStream.close();
+            inputFileChannel.close();
+
+            // convert the byte stream to byte array
+            fileContent = fileContentStream.toByteArray();
+        } catch (FileNotFoundException e1) {
+            PrintMessage.e("Open File", String.format("Failed to open file %s because it doesn't exist", filename));
+            return false;
         } catch (IOException e) {
-            // TODO Auto-generated catch block
+            PrintMessage.e("Open File", String.format("An IO error ocurred while reading the file %s", filename));
             e.printStackTrace();
+            return false;
         }
 
+        // request peers in the chord network to store the file
+        // attempt to meet the desired replication degree
         OurFile ourFile = new OurFile(filename, replicationDegree);
 
         for(int i = 0 ; i < replicationDegree ; i++) {
             ChordKey key = new ChordKey(filename, i);
 
-            boolean success = node.putObj(key, content);
+            boolean success = node.putObj(key, fileContent);
             if (success) {
                 ourFile.getFileDegreeKey().put(i, key);
             } else {
@@ -93,14 +129,14 @@ public class RMIListen implements RMIInterface {
 
     @Override
     public Boolean delete(String filename) throws RemoteException {
-        for(int i = 0 ; i < this.node.getfNameKeys().get(filename).getTotalReplicationDegree() ; i++) {
+        for (int i = 0; i < this.node.getfNameKeys().get(filename).getTotalReplicationDegree(); i++) {
             ChordKey key = this.node.getfNameKeys().get(filename).getFileDegreeKey().get(i);
             boolean success = node.delObj(key);
             if (success) {
                 this.node.getfNameKeys().get(filename).getFileDegreeKey().remove(i);
             }
         }
-        if(this.node.getfNameKeys().get(filename).getFileDegreeKey().size() == 0) {
+        if (this.node.getfNameKeys().get(filename).getFileDegreeKey().size() == 0) {
             this.node.delFileNameKeyPair(filename);
         }
         return true;
@@ -110,7 +146,7 @@ public class RMIListen implements RMIInterface {
     public String info() throws RemoteException {
         // get State
         var wrapper = new Object() {
-            public String res ="";
+            public String res = "";
         };
 
         wrapper.res += "Backed up files:" + System.lineSeparator();
